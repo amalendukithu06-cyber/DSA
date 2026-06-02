c111d598efce59628f64d95b9557a731d7162b9b
amalendukithu06-cyber <amalendukithu06@gmail.com>
2026-06-02 20:29:11 +0530

    Session 2.py

--- Session 2.py
+++ Session 2.py
@@ -1,3 +1,27 @@
-print ("Microsoft word sgortcuts")
-print("Screenshots")
-print("Chrome")
+Windows + H - Microsoft word voice to text by typing
+
+Windows + dot - emojis
+
+NotebookLm - paragraph edit or chat
+
+Ctrl + A - Select All
+Ctrl + C - Copy
+Ctrl +  V -  Paste
+Ctrl + Z - Undo
+Ctrl + Y - Redo
+Ctrl + shift + arrow keys  - Select text
+Ctrl + P - Print
+Ctrl + F - Find
+Ctrl + H - Find & Replace
+Ctrl + Shift + T - to get back the tab
+Ctrl + W - close tab
+
+Print screen - ScreenShot
+
+Windows + Printscreen - Full Screenshot
+
+Printdcreen + video - Screen Recording
+
+Windows + Alt + R - Full Screen record
+
+Windows + G - Gaming setup 
