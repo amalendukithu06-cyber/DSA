3bc53b35ef2c7bb0753f3b325f4af2d54ee2766f
amalendukithu06-cyber <amalendukithu06@gmail.com>
2026-06-02 20:35:14 +0530

    Session 4.py

---
+++ Session 4.py
@@ -0,0 +1,13 @@
+21 May 2026
+12:09
+
+NumPy is a python library for large multi-dimensional array processing
+
+
+Pandas -> Built on top of NumPy, which provides support for multi-dimensional array
+
+
+Scikit (Science kit) Learn Machine Learning Library in Python.
+Package manager ----> PIP
+
+VENV --> to create virtual environment in python
