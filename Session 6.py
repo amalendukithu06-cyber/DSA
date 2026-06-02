dd2da8b124368c019e3807545a0c4a9941a1a686
amalendukithu06-cyber <amalendukithu06@gmail.com>
2026-06-02 20:38:57 +0530

    Session 6.py

---
+++ Session 6.py
@@ -0,0 +1,43 @@
+Operators
+
+25 May 2026
+12:31
+
+Decimal & Binary Numbers
+
+Assignment Operators
+
+Arithmetic 
+
+Addition
+Subtraction
+Multiplication
+Division
+Exponential
+Floor division
+Modulus
+
+
+Relational
+
+Greater than 
+Less than
+Equal to
+Greater the or equal to
+Less than or equal to
+
+
+Logical 
+
+AND
+OR
+NOT
+
+	Paatshala ---> session 8
+
+Iterative Statements
+2 types
+
+Definite loop        &        Indefinite loop
+
+	• For  loop                * While loop       
