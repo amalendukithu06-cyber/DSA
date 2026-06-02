8c97d4caef3509d3855e1f8910130c44faba4cbe
amalendukithu06-cyber <amalendukithu06@gmail.com>
2026-06-02 20:26:13 +0530

    Session 1.py

--- Session 1.py
+++ Session 1.py
@@ -1,4 +1,14 @@
-print("Life Skills")
-print("Ctrl + shift + esc - processes")
-print("Windows shortcuts")
-print("Control keys shortcuts")
++Life skills
++ Ctrl + shift + esc - processes
+
++ Alt+fn+f4 - close tab
+
++ Ctrl + alt + delete - avoid hang/stuck
+
++Windows + tab/ Alt + tab - switch tab
+
++Windows + R - Run dialogue
+
++ Voice typing is possible in Notepad
+
++ Windows + X  -  Details 
