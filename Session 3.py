89fc7475ce828ebdf5170dd80e3f58f787e948c1
amalendukithu06-cyber <amalendukithu06@gmail.com>
2026-06-02 20:33:08 +0530

    Session 3.py

--- Session 3.py
+++ Session 3.py
@@ -1,3 +1,67 @@
-print("Applications of DSA")
-print("Dr ignaz Semmelvies")
-print("Machine learning")
+Chrome -> docs.new - new  document
+
+   paragraph -> ctrl+shift+J - for justificstion
+                          ctrl+shift+coma - text size
+                         Ctrl+shift+U - center allign
+                        Ctrl+Alt+2 - Heading size enlarge
+                     
+
+
+
+
+
+
+You can open the site -> copy link & ctrl+K
+Foot note -> ctrl+Alt+F and Ctrl+slash+F
+Application of DSA
+
+21 May 2026
+09:42
+
+Health Care
+
+Diseases Prediction
+Medication Effectiveness
+
+Dr. Ignaz 
+ Semmelweis
+
+From <https://en.wikipedia.org/wiki/Ignaz_Semmelweis> 
+
+Social Media
+
+Sentiment Analysis
+Digital Marketing
+
+
+Marketing
+
+Upselling
+Cross Selling
+Predicting lifetime
+Value of consumer
+chum
+
+Sales
+
+Discount Offering
+Demand Forecasting
+
+Travel
+
+Machine Learning
+
+Dynamic Pricing
+Predicting Flight delay
+
+Automation
+
+Self-Driving cars
+Pilotless aircrafts ,drones
+
+Credit & Insurance
+
+Claims Prediction
+Fraud & Risk Detection
+
+
