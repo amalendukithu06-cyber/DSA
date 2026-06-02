a7ca6f9ee493fa0a45e88f47f721c69d8104508f
amalendukithu06-cyber <amalendukithu06@gmail.com>
2026-06-02 20:37:15 +0530

    Session 5.py

---
+++ Session 5.py
@@ -0,0 +1,43 @@
+Python
+
+25 May 2026
+09:51
+
+String -----> sequence of characters
+Text[0] ------> fetch 1st letter
+Text[-1] ------> fetch last character
+
+Guido van Rossam --> father of python
+
+Object Oriented program
+High level
+Interpreted
+Dynamic Semantics
+Extension ----> .py
+
+
+
+Texts are represented using quotes(string)
+
+
+
+
+
+
+Variables -----> A reserved memory location to store values
+Variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alphanumeric characters 
+
+KEYWORDS
+
+Good variable Name
+
+Choose meaningful name instead of short name roll_no -----> rn.
+Maintain the length of a variable.
+
+
+Format function - string ----> variable
+
+Input value insert cheyyan vndi
+
